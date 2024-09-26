a8d04ada570e23aed431a17d2e9d37c4ca08453c
Samuel Olusanya <164627820+tech-happen@users.noreply.github.com>
2024-09-26 13:23:40 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        coolvetica: ['Coolvetica', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+  
