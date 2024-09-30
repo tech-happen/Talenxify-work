80fafabb4146ae59e4b28dd04cb6e41a0ce9741a
Samuel Olusanya <164627820+tech-happen@users.noreply.github.com>
2024-09-30 11:30:32 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import { useState } from "react";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Features from "./pages/Features";
+import Contact from "./pages/Contact";
+import NavBar from "./components/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/features" element={<Features />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
+//Setup React Router Dom
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Buttons = () => {
-  return <div>Buttons</div>;
+  return (
+    <div className="flex items-center space-x-4 mr-[-390px] hidden md:block">
+      <Link
+        to="/signup"
+        className="bg-blue-600 text-white px-20 py-4  text-1xl font-semibold font-Coolvetica rounded-full hover:bg-blue-700 transition duration-300"
+      >
+        SIGN UP
+      </Link>
+      <Link
+        to="/login"
+        className="bg-white text-blue-800 px-20 py-4  text-1xl font-semibold rounded-full border border-blue-800 hover:bg-blue-50 transition duration-300"
+      >
+        LOGIN
+      </Link>
+    </div>
+  );
 };
 
 export default Buttons;
