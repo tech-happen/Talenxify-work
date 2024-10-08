eb6105558ef851baf955222d99cdd44ea1553deb
Adeyeye Olalekan <adeyeye.lekan01@gmail.com>
2024-10-08 20:56:44 +0100

    added seo component for all pages

---
+++ src/components/seo/seo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Helmet } from "react-helmet-async";
+export default function SEO({ title, description, type }) {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta property="og:type" content={type} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+    </Helmet>
+  );
+}
